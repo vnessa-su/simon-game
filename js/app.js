3799b2dfae156cdde4102e3bbf02caf37fe1fb55
Vanessa Su <vnessa.su@gmail.com>
2021-08-20 10:09:52 -0400

    :hammer: Generate random sequence and higlight game buttons in that sequence

--- js/app.js
+++ js/app.js
@@ -16,5 +16,26 @@ const generateGameButtons = (numberOfButtons) => {
     }
 };
 
+const generateRandomPattern = (sequenceLength, numberOfButtons) => {
+    const sequence = [];
+    for (let i = 0; i < sequenceLength; i++) {
+        const randomButtonNumber = Math.floor(Math.random() * numberOfButtons);
+        sequence.push(randomButtonNumber);
+    }
+    console.log(sequence);
+    return sequence;
+};
+
+const highlightButtonsInSequence = () => {
+    const sequenceOfButtons = generateRandomPattern(4, 4);
+    sequenceOfButtons.forEach((buttonId, sequenceIndex) => {
+        setTimeout(() => {
+            const buttonToHighlight = document.getElementById(buttonId);
+            buttonToHighlight.focus();
+        }, 1000 * sequenceIndex);
+    });
+};
+
 createGameStartButton();
 generateGameButtons(4);
+highlightButtonsInSequence();
